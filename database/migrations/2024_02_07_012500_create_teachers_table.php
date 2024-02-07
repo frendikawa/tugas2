c656f623bbab0aec6df6a897f24addbbdf9aae4b
frendikawa <frsnaifla@gmail.com>
2024-02-07 13:40:57 +0700

    addData

--- database/migrations/2024_02_07_012500_create_teachers_table.php
+++ database/migrations/2024_02_07_012500_create_teachers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('gender',['male','female']);
             $table->timestamps();
         });
     }
